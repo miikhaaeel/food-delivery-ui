9ee561bd89538a66fa911e9f4247addbbcc98882
Miikhaaeel <fadowomichael1@gmail.com>
2021-12-10 14:49:07 +0100

    Stack widget and hero animation

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_ui/models/restaurant.dart';
 
-class RestaurantScreen extends StatelessWidget {
+class RestaurantScreen extends StatefulWidget {
   final Restaurant restaurant;
-  const RestaurantScreen({Key? key, required this.restaurant}) : super(key: key);
+  const RestaurantScreen({Key? key, required this.restaurant})
+      : super(key: key);
 
   @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Hero(tag: widget.restaurant.imageUrl,
+                child: Image(
+                  height: 220.0,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    widget.restaurant.imageUrl,
+                  ),
+                ),
+              ),
+              SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite),
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 35.0,
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
