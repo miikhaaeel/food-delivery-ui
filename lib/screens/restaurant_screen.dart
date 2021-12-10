c202d0a437b27e3787dada82f0b77302df790ce2
Miikhaaeel <fadowomichael1@gmail.com>
2021-12-10 16:06:25 +0100

    restaurant details

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_ui/models/restaurant.dart';
+import 'package:food_delivery_ui/widgets/rating_stars.dart';
 
 class RestaurantScreen extends StatefulWidget {
   final Restaurant restaurant;
@@ -18,7 +19,8 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
         children: [
           Stack(
             children: [
-              Hero(tag: widget.restaurant.imageUrl,
+              Hero(
+                tag: widget.restaurant.imageUrl,
                 child: Image(
                   height: 220.0,
                   width: MediaQuery.of(context).size.width,
@@ -36,19 +38,88 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                       onPressed: () {
                         Navigator.pop(context);
                       },
-                      icon: Icon(Icons.arrow_back),
+                      icon: const Icon(Icons.arrow_back),
                       color: Colors.white,
                       iconSize: 30.0,
                     ),
                     IconButton(
                       onPressed: () {},
-                      icon: Icon(Icons.favorite),
+                      icon: const Icon(Icons.favorite),
                       color: Theme.of(context).primaryColor,
                       iconSize: 35.0,
                     )
                   ],
                 ),
-              )
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.restaurant.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 22,
+                        letterSpacing: -1,
+                      ),
+                    ),
+                    const Text(
+                      '0.2 miles away',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                RatingStars(ratings: widget.restaurant.rating),
+                const SizedBox(height: 8),
+                Text(
+                  widget.restaurant.address,
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Reviews',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Contact',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
             ],
           )
         ],
